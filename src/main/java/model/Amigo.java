4c526a4b40a88ba4e3566cf94e2890afdb08d3a8
1072419595 <1072419595@UNS11DT099054.animaedu.intranet>
2024-05-14 10:11:00 -0300

    feat: adição de comentarios na class "Amigo"

--- src/main/java/model/Amigo.java
+++ src/main/java/model/Amigo.java
@@ -5,22 +5,29 @@ import java.util.ArrayList;
 
 public class Amigo extends Base {
 
-    //atributos
+    /**
+     * atributos
+     */
     private String telefone;
     AmigoDAO dao;
 
-    //construtor
+    /**
+     * construtor
+     */
     public Amigo() {
-        this(0,"","");
+        this(0, "", "");
     }
 
     public Amigo(int id, String nome, String telefone) {
         super(id, nome);
         this.telefone = telefone;
-        
+
         dao = new AmigoDAO();
     }
-    //get e set
+
+    /**
+     * get e set
+     */
     public String getTelefone() {
         return telefone;
     }
@@ -29,38 +36,48 @@ public class Amigo extends Base {
         this.telefone = telefone;
     }
 
-    //Método toString para imprimir os detalhes do Amigo
+    /**
+     * Método toString para imprimir os detalhes do Amigo
+     */
     @Override
     public String toString() {
         return super.toString() + "telefone=" + telefone;
     }
-    
+
     public ArrayList<Amigo> getMinhaLista() {
         return dao.getMinhaLista();
     }
 
-    // Cadastra novo amigo
+    /**
+     * Cadastra novo amigo
+     */
     public boolean insertAmigoBD(String nome, String telefone) {
-        int id = dao.maiorID() +1;
+        int id = dao.maiorID() + 1;
         Amigo objeto = new Amigo(id, nome, telefone);
         dao.insertAmigoBD(objeto);
         return true;
     }
 
-    // Deleta um amigo especÍfico pelo seu campo ID
+    /**
+     * Deleta um amigo especÍfico pelo seu campo ID
+     */
     public boolean deleteAmigoBD(int id) {
         dao.deleteAmigoBD(id);
         return true;
     }
 
-    // Edita um amigo especÍfico pelo seu campo ID
+    /**
+     * Edita um amigo especÍfico pelo seu campo ID
+     */
     public boolean updateAmigoBD(int id, String nome, String telefone) {
         Amigo objeto = new Amigo(id, nome, telefone);
         dao.updateAmigoBD(objeto);
         return true;
     }
 
-    // procura o INDICE de objeto da MinhaLista que contem o ID enviado.
+    /**
+     * procura o INDICE de objeto da MinhaLista que contem o ID enviado.
+     */
     private int procuraIndice(int id) {
         int indice = -1;
         for (int i = 0; i < dao.minhaLista.size(); i++) {
@@ -70,18 +87,20 @@ public class Amigo extends Base {
         }
         return indice;
     }
-    
-    // carrega dados de um amigo especÍfico pelo seu ID
+
+    /**
+     * carrega dados de um amigo especÍfico pelo seu ID
+     */
     public Amigo carregaAmigo(int id) {
         int indice = this.procuraIndice(id);
         return dao.minhaLista.get(indice);
     }
-    
-    // retorna o maior ID da nossa base de dados
-    public int maiorID(){
+
+    /**
+     * retorna o maior ID da nossa base de dados
+     */
+    public int maiorID() {
         return dao.maiorID();
-    }   
-    
+    }
+
 }
-
-
