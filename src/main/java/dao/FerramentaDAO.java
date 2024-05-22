70f6b0ee4173035929ec5c4c9eca11c42c0ee3c0
turt0 <[caio2006cunha@gmail.com] (mailto:caio2006cunha@gmail.com)>
2024-05-22 09:31:55 -0300

    git config user.name "turt0" git config user.email "[caio2006cunha@gmail.com](mailto:caio2006cunha@gmail.com)"

--- src/main/java/dao/FerramentaDAO.java
+++ src/main/java/dao/FerramentaDAO.java
@@ -7,7 +7,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-import model.Ferramenta;
+import modelo.Ferramenta;
 
 public class FerramentaDAO extends BaseDAO {
 
